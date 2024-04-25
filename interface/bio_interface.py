c5955ccc0c437008936e42e141f530ef6730ffb6
Ike <75408516+IkeAny@users.noreply.github.com>
2024-04-25 18:44:09 -0400

    Update bio_interface.py

--- interface/bio_interface.py
+++ interface/bio_interface.py
@@ -7,34 +7,44 @@ def build_bio_interface():
             '''
             # Student Biographies
 
-            # Here is a list of all the students who worked on this interface
+            ## Here is a list of all the students who worked on this interface research project:
 
 
-            ## Craig Droke
+            ### Craig Droke
 
-            ### Junior Electrical and Computer Engineering major at Rowan University. Work includes image detection and UI/UX development.
+            A junior Electrical and Computer Engineering major at Rowan University. Work includes image detection and UI/UX development.
 
-            ## Jimmy Galeno
+            ### Jimmy Galeno
 
-            ### I am a Junior Electrical and Computer Engineering student at Rowan University interested in Power Systems Engineering. Portion of my work includes model and interface settings.
+            A junior Electrical and Computer Engineering student at Rowan University interested in Power Systems Engineering. Portion of my work includes model and interface settings.
 
-            ## Christian Cipolletta
+            ### Christian Cipolletta
 
-            ### I am a Junior Electrical & Computer Engineering Student at Rowan University interested in Machine Learning. My work includes the model training UI development.
+            A junior Electrical & Computer Engineering Student at Rowan University interested in Machine Learning. My work includes the model training UI development.
 
-            ## Ozan Tekben
+            ### Ozan Tekben
 
-            ### Senior Electrical and Computer Engineering Major at Rowan University. Work includes project management and interface testing.
+            A senior Electrical and Computer Engineering Major at Rowan University. Work includes project management and interface testing.
 
-            ## Matt O'Donnell
+            ### Matt O'Donnell
 
-            ### I am a Junior Electrical and Computer Engineering student at Rowan University interested in Embedded Systems development. My work includes model detection settings and interface design.
+            A junior Electrical and Computer Engineering student at Rowan University interested in Embedded Systems development. My work includes model detection settings and interface design.
 
-            ## Chinedu Ike-Anyanwu
+            ### Chinedu Ike-Anyanwu
 
-            ### Senior Electrical and Computer Engineering Major at Rowan University. Work includes model training and interface development.
+            A senior Electrical and Computer Engineering Major at Rowan University. Works includes model training and interface development. Strives to further his education in robotics and AI.
 
+            ### Ayo Overton 
+            
+            A Junior Electrical and Computer Engineering student at Rowan University interested in Circuit Design, Implementation, and Development. Portion of my work includes documentation and additional testing.
+            
+            ### Asien Truong
+            
+            Junior Mechanical Engineering Major at Rowan University interested in software development and aerospace engineering. Portion of my work includes adversarial attack training and documentation.
 
+            ### Kaitlyn Pounds
+            
+            Junior electrical and computer engineer. Work includes information organization/presentation and additional documentation.
             
 
 
